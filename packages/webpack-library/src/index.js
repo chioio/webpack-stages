3c33dbd11552571ac612711da26ef92f8b48f1e1
Tenn Chio <Tenn_Chio@icloud.com>
2020-11-03 17:49:36 +0800

    ☺︎ Webpack: Authoring Libraries

---
+++ packages/webpack-library/src/index.js
@@ -0,0 +1,3 @@
+import * as libraryNumbers from 'webpack-numbers';
+
+console.log(libraryNumbers.word('Two'));
---
+++ packages/webpack-library/webpack-numbers/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'webpack-numbers.js',
+    library: 'webpackNumbers',
+    libraryTarget: 'umd',
+  },
+  externals: {
+    /**
+     * This means that your library expects a dependency
+     * named `lodash` to be available in the consumer's
+     * environment.
+     */
+    lodash: {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      root: '_'
+    }
+  }
+};
